54433e38c1d5418ebd967dc2d44f24bdb1495813
alexey89 <traf777@mail.ru>
2018-03-14 21:53:03 +0300

    Exclude properties of LicenceKeyPartAttribute from obfuscation

--- ExcelReportGenerator/Attributes/LicenceKeyPartAttribute.cs
+++ ExcelReportGenerator/Attributes/LicenceKeyPartAttribute.cs
@@ -1,14 +1,18 @@
 ﻿using System;
+using System.Reflection;
 
 namespace ExcelReportGenerator.Attributes
 {
     [AttributeUsage(AttributeTargets.Interface | AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum | AttributeTargets.Method | AttributeTargets.Property)]
     internal class LicenceKeyPartAttribute : Attribute
     {
+        [Obfuscation(Exclude = true, Feature = "renaming")]
         public bool L { get; set; }
 
+        [Obfuscation(Exclude = true, Feature = "renaming")]
         public bool R { get; set; }
 
+        [Obfuscation(Exclude = true, Feature = "renaming")]
         public bool U { get; set; }
     }
 }
