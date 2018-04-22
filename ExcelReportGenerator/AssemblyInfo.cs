a610f79b0d2f0813000dd617eae474d4ab8fffbc
alexey89 <traf777@mail.ru>
2018-04-22 17:42:23 +0300

    NuGet package settings

--- ExcelReportGenerator/AssemblyInfo.cs
+++ ExcelReportGenerator/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("ExcelReportGenerator.Tests")]
-[assembly: InternalsVisibleTo("ExcelReportGenerator.LicenseManager")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
